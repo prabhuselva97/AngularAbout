881fb646c45673c26ab04cdffede06002ac88183
Prabhu S <prabhu@virtusa.com>
2018-06-13 17:53:58 +0530

    First Commit

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'application-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent  {
+
+  
+
+}
